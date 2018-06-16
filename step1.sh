77952160485070f8f3ce776eee09850d8c47ef59
Callan05 <weedogg@gmail.com>
2018-06-16 15:51:59 +1000

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,6 +1,6 @@
 # Download the latest Alpha build to the upgrades folder
 
 #assuming RPI2/3
-wget http://milhouse.libreelec.tv/builds/master/RPi2/LibreELEC-RPi2.arm-9.0-Milhouse-20180615210304-%230615-g83c436f.tar -O /storage/.updates/Milhouse.tar
+wget http://milhouse.libreelec.tv/builds/master/RPi2/LibreELEC-RPi2.arm-9.0-Milhouse-20180615210304-%230615-g83c436f.tar -O /storage/.update/Milhouse.tar
 
 shutdown -r now
