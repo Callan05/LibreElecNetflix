f0fef2ca173decb410b570fdd627af689faf4b62
Callan05 <weedogg@gmail.com>
2018-06-16 16:58:13 +1000

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,7 +1,7 @@
 #wget https://github.com/kodinerds/repo/raw/master/repository.netflix/repository.netflix-1.0.1.zip -O /storage/repository.netflix-1.0.1.zip
-wget wget https://github.com/asciidisco/plugin.video.netflix/archive/master.zip -O netflix.zip
+wget https://github.com/asciidisco/plugin.video.netflix/archive/master.zip -O netflix.zip
 
 #pause here and install netflix plugin
-  wget http://nmacleod.com/public/libreelec/getwidevine.sh
+    wget http://nmacleod.com/public/libreelec/getwidevine.sh
   sh getwidevine.sh
   shutdown -r now
