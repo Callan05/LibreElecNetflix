17e59a08af932433f48539fad0657354b1f7c26f
Callan05 <weedogg@gmail.com>
2018-06-16 17:01:58 +1000

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,7 +1,10 @@
+#unsure why the netflix plugin from the repository does not work - as at June 16 2018, we need to manually grab the plugin
 #wget https://github.com/kodinerds/repo/raw/master/repository.netflix/repository.netflix-1.0.1.zip -O /storage/repository.netflix-1.0.1.zip
 wget https://github.com/asciidisco/plugin.video.netflix/archive/master.zip -O netflix.zip
 
 #pause here and install netflix plugin
+# can we do this from CLI ?
+
     wget http://nmacleod.com/public/libreelec/getwidevine.sh
   sh getwidevine.sh
   shutdown -r now
